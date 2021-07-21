71df7db9b35c945054dd79c2eb0d918ed6b7406c
Thana <thanailde.oliveira@outlook.com>
2021-07-21 11:31:48 -0300

    Implementando novas funções na página com JavaScript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,16 +15,20 @@ por seletor: quary Selector()
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
 
 nome.style.width = '100%'
 email.style.width = '100%'
 
-function validaAssunto(){
+/*function validaAssunto(){
 
     if(assunto.value.length < 3){
         alert('Assunto inválido! Explique melhor o assunto.')
     }
-}
+}*/
 
 function validaNome(){
     let txtNome = document.querySelector('#txtNome')
@@ -35,6 +39,7 @@ function validaNome(){
     else{
         txtNome.innerHTML = 'Nome válido!'
         txtNome.style.color = 'green'
+        nomeOk = true
     }
 }
 
@@ -48,6 +53,41 @@ function validaEmail(){
     else{
         txtEmail.innerHTML = '✔'
         txtEmail.style.color = 'green'
+        emailOk = true
     }
     
 }
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if(assunto.value.length >= 280){
+        txtAssunto.innerHTML = 'O texto é muito grande, digite no máximo 280 caracteres!'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    }
+    else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar (){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Formulário enviado com sucesso!')
+    }
+    else{
+        alert('Preencha o formulário corretamente antes de enviar.')
+    }
+}
+
+function mapaZoom (){
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal (){
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
+
